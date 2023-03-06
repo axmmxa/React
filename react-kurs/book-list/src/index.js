c040b6424a89711eb94a44921bc1fef09e080b64
Alexander Much <alexandermuch@Christophs-Mini.fritz.box>
2023-03-06 11:48:07 +0100

    amazone booklist

--- react-kurs/book-list/src/index.js
+++ react-kurs/book-list/src/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const Booklist = () => {
+  return(
+    <section>
+      <Book></Book>
+      <Book></Book>
+      <Book></Book>
+      <Book></Book>
+    </section>
+  )
+}
+
+const Book = () =>{
+  return <article>
+    <Image></Image>
+    <Title></Title>
+    <Author></Author>
+  </article>
+}
+
+const Image = () => <img src='https://images-na.ssl-images-amazon.com/images/I/81a4bSDHqUL._AC_UL600_SR600,400_.jpg'></img>
+const Title = () => <h2>Daisy Jones & The Six</h2>
+const Author = () => {
+  return <h4>Taylor Jenkins Reid</h4>
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ <Booklist></Booklist>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
