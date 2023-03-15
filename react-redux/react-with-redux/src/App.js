6e394ece0be9d48bf15d3a9593ba5a0846fea399
Alexander Much <46798654+axmmxa@users.noreply.github.com>
2023-03-15 17:29:17 +0100

    finished with bugs

--- react-redux/react-with-redux/src/App.js
+++ react-redux/react-with-redux/src/App.js
@@ -1,16 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
-import { useSelector} from "react-redux"
+import { useSelector, useDispatch} from "react-redux"
+import { bindActionCreators} from "redux"
+import { actionCreators} from"./state/index"
+
 
 function App() {
 
   const state = useSelector((state) => state)
+  const dispatch = useDispatch()
+
+  const { depositMoney, withdrawMoney } = bindActionCreators(actionCreators, dispatch);
+
+  console.log(AC)
+
 
   console.log(state)
 
   return (
     <div className="App">
-    
+      <h1>account</h1>
+      <button onClick={() => depositMoney(100)}> Deposit</button>
+      <button onclick= {() => withdrawMoney(20)}> Withdraw</button>
     </div>
   );
 }
