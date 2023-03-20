02558135db78d1ea6f262bd5c16001e0f32d080e
Alexander Much <alexandermuch@Christophs-Mini.fritz.box>
2023-03-20 09:49:11 +0100

    update

--- react-kurs/book-list/src/index.js
+++ react-kurs/book-list/src/index.js
@@ -23,7 +23,7 @@ const Book = () =>{
   </article>
 }
 
-const Image = () => <img src='https://images-na.ssl-images-amazon.com/images/I/81a4bSDHqUL._AC_UL600_SR600,400_.jpg'></img>
+const Image = () => <img src='./images/book1.jpg'></img>
 const Title = () => <h2>Daisy Jones & The Six</h2>
 const Author = () => {
   return <h4>Taylor Jenkins Reid</h4>
