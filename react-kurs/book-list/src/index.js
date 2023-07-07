c5e18db7dfb753767639652336fa29cec7eaac60
Alexander Much <alexandermuch@Christophs-Mini.fritz.box>
2023-07-07 10:33:03 +0200

    first component

--- react-kurs/book-list/src/index.js
+++ react-kurs/book-list/src/index.js
@@ -1,42 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react"
+import  ReactDOM  from "react-dom/client"
 
 
-import './index.css'
-
-const title = "Intersting facts For Curious Minds";
-const author = "Jordan More"
-const img = "./images/book1.jpg"
-
-
-const Booklist = () => {
-  return(
-    <section className='booklist'>
-      <Book author={author} title={title} img={img}></Book>
-      <Book author={author} title={title} img={img}></Book>
-    </section>
-  )
+function Greeting() {
+    return <h2>My first Component</h2>
 }
 
+const root = ReactDOM.createRoot(document.getElementById('root'))
 
-
-const Book = (props) =>{
-  return <article className='book'>
-    <img src={props.img}/>
-    <h2>{props.title}</h2>
-    <h4>{props.author}</h4>
-    <p></p>
-  </article>
-}
-
-
-
-
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
- <Booklist></Booklist>
-);
-
-
+root.render(<Greeting></Greeting>)
