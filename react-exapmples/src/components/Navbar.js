8986af32f5f6585377d961cc2a86a97e0e6471b8
Alexander Much <46798654+axmmxa@users.noreply.github.com>
2023-07-29 13:29:08 +0200

    react-navbar

---
+++ react-exapmples/src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header>
+        <h3>logo</h3>
+        <nav>
+            <a href='/#'>Home</a>
+            <a href='/#'>MyWork</a>
+            <a href='/#'>Bloq</a>
+            <a href='/#'>About me</a>
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
