3ddc61a4c1852683dd8c703c28032ccaa024e2dc
Alexander Much <46798654+axmmxa@users.noreply.github.com>
2023-07-29 17:15:14 +0200

    react header to examples added

--- react-exapmples/src/components/Navbar.js
+++ react-exapmples/src/components/Navbar.js
@@ -1,15 +1,39 @@
 import React from 'react'
+import {useRef} from 'react'
+
+import '../styles/main.css'
+
+import{FaBars, FaTimes} from 'react-icons/fa'
 
 const Navbar = () => {
+
+    const navRef = useRef()
+
+    const showNavbar = () => {
+        navRef.current.classList.toggle('responsive_nav')
+        console.log("Test")
+    }
+
   return (
     <header>
+
         <h3>logo</h3>
-        <nav>
+
+        <nav ref={navRef}>
             <a href='/#'>Home</a>
             <a href='/#'>MyWork</a>
             <a href='/#'>Bloq</a>
             <a href='/#'>About me</a>
+
+            <button className='nav-btn nav-close-btn' onClick={showNavbar}>
+                <FaTimes></FaTimes>
+            </button>
         </nav>
+
+        <button className='nav-btn' onClick={showNavbar}>
+            <FaBars></FaBars>
+        </button>
+
     </header>
   )
 }
